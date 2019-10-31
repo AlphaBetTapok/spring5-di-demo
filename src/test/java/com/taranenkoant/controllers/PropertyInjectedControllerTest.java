7fbfef4feb70100ec95426fbdc55d949573c0283
Антон Тараненко <taranenkoant@gmail.com>
2019-10-31 16:56:35 +0300

    initial commit

---
+++ src/test/java/com/taranenkoant/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package com.taranenkoant.controllers;
+
+import guru.services.GreetingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+public class PropertyInjectedControllerTest {
+
+    private PropertyInjectedController propertyInjectedController;
+
+    @Before
+    public void setUp() throws Exception {
+        this.propertyInjectedController = new PropertyInjectedController();
+        this.propertyInjectedController.greetingServiceImpl = new GreetingServiceImpl();
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, propertyInjectedController.sayHello());
+    }
+}
